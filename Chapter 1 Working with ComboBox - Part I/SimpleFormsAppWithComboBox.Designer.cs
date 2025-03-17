e90def3050312d5d4b9788543cb4bd99c4f7f600
BrentJohnPaller <gamehunter123100@gmail.com>
2025-03-17 10:34:12 +0800

    Add files via upload

---
+++ Chapter 1 Working with ComboBox - Part I/SimpleFormsAppWithComboBox.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace SimpleFormsAppWithComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cuisinecomboBox1 = new System.Windows.Forms.ComboBox();
+            this.cuisinecheckedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.cuisineListBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(427, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SIMPLE FAVORITE FOOD PICKER APP";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cuisine";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(245, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Food List";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(465, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "My Favorite Food List";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // cuisinecomboBox1
+            // 
+            this.cuisinecomboBox1.FormattingEnabled = true;
+            this.cuisinecomboBox1.Location = new System.Drawing.Point(12, 132);
+            this.cuisinecomboBox1.Name = "cuisinecomboBox1";
+            this.cuisinecomboBox1.Size = new System.Drawing.Size(121, 21);
+            this.cuisinecomboBox1.TabIndex = 4;
+            this.cuisinecomboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // cuisinecheckedListBox1
+            // 
+            this.cuisinecheckedListBox1.FormattingEnabled = true;
+            this.cuisinecheckedListBox1.Location = new System.Drawing.Point(248, 133);
+            this.cuisinecheckedListBox1.Name = "cuisinecheckedListBox1";
+            this.cuisinecheckedListBox1.Size = new System.Drawing.Size(157, 184);
+            this.cuisinecheckedListBox1.TabIndex = 5;
+            this.cuisinecheckedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // cuisineListBox1
+            // 
+            this.cuisineListBox1.FormattingEnabled = true;
+            this.cuisineListBox1.Location = new System.Drawing.Point(468, 133);
+            this.cuisineListBox1.Name = "cuisineListBox1";
+            this.cuisineListBox1.Size = new System.Drawing.Size(175, 186);
+            this.cuisineListBox1.TabIndex = 6;
+            this.cuisineListBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 33);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(248, 323);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 31);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(649, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 36);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Remove";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(649, 175);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 35);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Clear All";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cuisineListBox1);
+            this.Controls.Add(this.cuisinecheckedListBox1);
+            this.Controls.Add(this.cuisinecomboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "SimpleFormsAppWithComboBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cuisinecomboBox1;
+        private System.Windows.Forms.CheckedListBox cuisinecheckedListBox1;
+        private System.Windows.Forms.ListBox cuisineListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Chapter 1 Working with ComboBox - Part I/SimpleFormsAppWithComboBox.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleFormsAppWithComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] cuisineGenre = { "Filipino", "Chinese", "German", "Italian" };
+            cuisinecomboBox1.Items.AddRange(cuisineGenre);
+
+            cuisinecomboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            cuisinecomboBox1.SelectedIndex = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] filipinoCuisineList = { "Pancit Canton", "Nilat-ang Baka", "Nguyong", "Fried Siomai", "Pritong Aso" };
+            string[] chineseCuisineList = { "Udon", "Shandong", "Sichuan", "Cantonese" };
+            string[] germanCuisineList = { "Schnitzel", "Currywurst", "Bratwurst" };
+            string[] italianCuisineList = { "Pasta", "Pizza", "Risotto", "Tiramisu" };
+
+            cuisinecheckedListBox1.Items.Clear();
+
+           if (cuisinecomboBox1.SelectedItem.ToString().Contains("Filipino")) {
+                cuisinecheckedListBox1.Items.AddRange(filipinoCuisineList);
+            }
+           else if
+              (cuisinecomboBox1.SelectedItem.ToString().Contains("Chinese")) {
+                cuisinecheckedListBox1.Items.AddRange(chineseCuisineList);
+            }
+           else if
+              (cuisinecomboBox1.SelectedItem.ToString().Contains("German")) {
+                cuisinecheckedListBox1.Items.AddRange(germanCuisineList);
+            }
+           else if
+              (cuisinecomboBox1.SelectedItem.ToString().Contains("Italian")) {
+                cuisinecheckedListBox1.Items.AddRange(italianCuisineList);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var item in cuisinecheckedListBox1.Items)
+            {
+                bool isSelected = cuisinecheckedListBox1.GetItemChecked(cuisinecheckedListBox1.Items.IndexOf(item));
+                if (isSelected && !cuisineListBox1.Items.Contains(item))
+                {
+                    cuisineListBox1.Items.Add(item);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (cuisineListBox1.Items.Count >= 1)
+            {
+                cuisineListBox1.Items.Remove(cuisineListBox1.SelectedItem);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            cuisineListBox1.Items.Clear();
+        }
+    }
+}
