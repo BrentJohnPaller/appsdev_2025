43af0f0de43a38d6feb045e178c23440335dab2d
BrentJohnPaller <gamehunter123100@gmail.com>
2025-05-18 20:36:25 +0800

    Add files via upload

---
+++ FINAL PROJECT APPSDEV DATA/MessProduct3.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace WindowsFormsAppMilkyWay
+{
+	partial class MessProduct3
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MessProduct3));
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.back = new System.Windows.Forms.Button();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.PaleTurquoise;
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(-2, 106);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(775, 100);
+			this.panel1.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.Teal;
+			this.label1.Location = new System.Drawing.Point(207, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(336, 55);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "CONTACT US";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(244, 256);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(262, 25);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Phone: (099) 9770917931";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(222, 298);
+			this.label4.Name = "label4";
+			this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.label4.Size = new System.Drawing.Size(307, 25);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "Address: Iloilo City, Philippines";
+			// 
+			// back
+			// 
+			this.back.AllowDrop = true;
+			this.back.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("back.BackgroundImage")));
+			this.back.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.back.FlatAppearance.BorderSize = 0;
+			this.back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.back.Location = new System.Drawing.Point(33, 32);
+			this.back.Name = "back";
+			this.back.Size = new System.Drawing.Size(40, 36);
+			this.back.TabIndex = 38;
+			this.back.UseVisualStyleBackColor = true;
+			this.back.Click += new System.EventHandler(this.back_Click);
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.label7);
+			this.panel2.Controls.Add(this.label6);
+			this.panel2.Controls.Add(this.label5);
+			this.panel2.Controls.Add(this.button1);
+			this.panel2.Controls.Add(this.panel3);
+			this.panel2.Controls.Add(this.label8);
+			this.panel2.Controls.Add(this.label9);
+			this.panel2.Controls.Add(this.label10);
+			this.panel2.Location = new System.Drawing.Point(1, 1);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(773, 925);
+			this.panel2.TabIndex = 39;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(207, 489);
+			this.label7.Name = "label7";
+			this.label7.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.label7.Size = new System.Drawing.Size(357, 25);
+			this.label7.TabIndex = 41;
+			this.label7.Text = " Metro Supermarket Ayala Mandaue";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(191, 448);
+			this.label6.Name = "label6";
+			this.label6.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.label6.Size = new System.Drawing.Size(388, 25);
+			this.label6.TabIndex = 40;
+			this.label6.Text = " Metro Supermarket Ayala Center Cebu";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(283, 392);
+			this.label5.Name = "label5";
+			this.label5.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.label5.Size = new System.Drawing.Size(190, 29);
+			this.label5.TabIndex = 39;
+			this.label5.Text = "Available Store";
+			// 
+			// button1
+			// 
+			this.button1.AllowDrop = true;
+			this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(33, 32);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(40, 36);
+			this.button1.TabIndex = 37;
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.Color.PaleTurquoise;
+			this.panel3.Controls.Add(this.label3);
+			this.panel3.Location = new System.Drawing.Point(0, 103);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(780, 100);
+			this.panel3.TabIndex = 0;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.Color.Teal;
+			this.label3.Location = new System.Drawing.Point(207, 22);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(336, 55);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "CONTACT US";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(97, 335);
+			this.label8.Name = "label8";
+			this.label8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.label8.Size = new System.Drawing.Size(572, 25);
+			this.label8.TabIndex = 3;
+			this.label8.Text = "Address:  Brgy. San Antonio, Makati City 1203, Philippines";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label9.Location = new System.Drawing.Point(245, 256);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(260, 25);
+			this.label9.TabIndex = 1;
+			this.label9.Text = "Phone: +63 917 626 1136";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label10.Location = new System.Drawing.Point(245, 294);
+			this.label10.Name = "label10";
+			this.label10.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.label10.Size = new System.Drawing.Size(260, 25);
+			this.label10.TabIndex = 2;
+			this.label10.Text = "Email: jck@arlafoods.com";
+			// 
+			// MessProduct3
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(773, 925);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.back);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "MessProduct3";
+			this.Text = "MessProduct3";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button back;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+	}
+}
